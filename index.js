3c5fa2abd40f25386e5926fba179ac13a858c43c
Luna Seifert <thepuddingdeity@gmail.com>
2022-09-30 12:53:48 -0600

    commit tree e7dddea30c10ea56382909e8601849c82166f7d8

--- index.js
+++ index.js
@@ -12,5 +12,21 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(string => {
+    let finalStringWithSpace = "" //test
+    let stringCut = string.split(" ")
+    let upperCasedWordArray = stringCut.map(word =>{
+      let firstLetter = word.substring(0, 1).toUpperCase()
+      let restOfWord = word.substring(1)
+      let finishedWord = firstLetter += restOfWord
+      return finishedWord
+    })
+
+    for (const word of upperCasedWordArray){
+      finalStringWithSpace += word + " " 
+    }
+    return finalStringWithSpace.substring(0, finalStringWithSpace.length - 1)
+  })
 }
+
+//console.log(titleCased)
